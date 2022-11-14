f13e674489ed66327ee12c1f20ea6b5380d84f47
ZemliakovD <112906798+ZemliakovD@users.noreply.github.com>
2022-11-14 09:49:29 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-$(function(){
-
+const panels = document.querySelectorAll('.panel');
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
+    });
 });
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    });
+}
