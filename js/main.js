ecfb09fff7ea05da0e891f1274d82e5f09eb44ff
Dmitri Zemliakov <112906798+ZemliakovD@users.noreply.github.com>
2023-02-08 17:19:29 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,26 +1,133 @@
-$('#images-demo').slickLightbox({
-  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636']
-});
-
-
 $(function(){
     $('.products-slider').slick({
         slidesToShow: 2,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2500,
-        arrows: false,
+        arrows: true,
+        prevArrow: '<button class="slick-prew products-slider-arrow"></button>',
+        nextArrow: '<button class="slick-next products-slider-arrow"></button>'
       });
 });
 
-let isVisible = true;
-const div = document.getElementById('ts1');
 
-function trigger() {
-  isVisible = !isVisible;
-  div.style.display = isVisible ? "block" : "none";
-};
+// Главная страница вкладка "Кухни"
 
-document
-  .getElementById('push')
-  .addEventListener('click', trigger);
+$('#kuh-1').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+$('#kuh-2').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+$('#kuh-3').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+$('#kuh-4').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+$('#kuh-5').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+$('#kuh-6').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+$('#kuh-7').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+$('#kuh-8').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+$('#kuh-9').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+// Главная страница вкладка "Фасады"
+
+$('#fasad-1').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+$('#fasad-2').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+
+$('#fasad-3').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+
+$('#fasad-4').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+
+$('#fasad-5').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+
+$('#fasad-6').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+
+$('#fasad-7').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+
+$('#fasad-8').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+
+$('#fasad-9').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+// Главная страница вкладка "Шкафы"
+
+$('#shkaf-1').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+$('#shkaf-2').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+$('#shkaf-3').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+$('#shkaf-4').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+$('#shkaf-5').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+$('#shkaf-6').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+$('#shkaf-7').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+$('#shkaf-8').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
+
+$('#shkaf-9').slickLightbox({
+  images: ['http://placekitten.com/1000/600', 'http://placekitten.com/1010/606', 'http://placekitten.com/1060/636'],
+});
